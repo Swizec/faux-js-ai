019f75e77d18d43ff5675b547ca2bfaada5005ac
Swizec Teller <swizec@swizec.com>
2017-05-21 15:19:18 -0700

    Basic evolution supposedly implemented

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 
 const _eval = require('eval');
 
+import _ from 'lodash';
+
 // We aim to maximize this value
 function fitness(code, conditions) {
     const N = conditions.length,
@@ -21,14 +23,20 @@ const Conditions = [
     (result, code) => code !== 4
 ];
 
-const CHARACTER_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\"'\`~!@#$%^&*()+=_-[]{},<>:;?/\\";
+const CHARACTER_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\"'\`~!@#$%^&*()+=_-[]{},<>:;?/\\ ";
 
+const MUTATE_FACTOR = 0.1,
+      MUTATE_LIKELIHOOD = 0.3;
+
+function randomChar() {
+    return CHARACTER_SET.charAt(Math.floor(Math.random() * CHARACTER_SET.length));
+}
 
 function memberMaker(memberLength) {
     let member = new Array(memberLength);
 
     for (let i = 0; i < memberLength; i++) {
-        member[i] = CHARACTER_SET.charAt(Math.floor(Math.random() * CHARACTER_SET.length));
+        member[i] = randomChar();
     }
 
     return member.join("");
@@ -44,6 +52,63 @@ function initialPopulation({ N, memberLength }) {
     return population;
 }
 
-let population = initialPopulation({ N: 50, memberLength: 50 });
+function rank(population, fitness) {
+    population.sort((a, b) => {
+        a = fitness(a, Conditions);
+        b = fitness(b, Conditions);
 
-console.log(population);
+        if (a > b) return 1;
+        if (a < b) return -1;
+        return 0;
+    });
+
+    return population;
+}
+
+function mutate(member) {
+    const N = member.length*MUTATE_FACTOR;
+
+    for (let i = 0; i < Math.round(N); i++) {
+        member[Math.floor(Math.random()*member.length)] = randomChar();
+    }
+
+    return member;
+}
+
+function breedPair(a, b) {
+    a = a.trim();
+    b = b.trim();
+
+    let child = a.slice(0, a.length/2) + b.slice(b.length/2);
+
+    if (Math.random() > MUTATE_LIKELIHOOD) {
+        child = mutate(child);
+    }
+
+    return child;
+}
+
+function breedPairs(pairs) {
+    return pairs.map(breedPair);
+}
+
+function* generation({ population, fitness, N }) {
+    while (true) {
+        population = rank(population, fitness);
+
+        // pairwise breed top 50% of population
+        population = population.concat(
+            breedPairs(_.chunk(_.take(population,
+                                 population.length/2),
+                          2)
+            ));
+
+        population = rank(population, fitness);
+
+        yield _.take(population, N);
+    }
+}
+
+
+
+let population = initialPopulation({ N: 50, memberLength: 50 });
